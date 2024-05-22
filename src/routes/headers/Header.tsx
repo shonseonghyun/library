5bdd068c6e3e680a53cce012a0fefd9c177dd75a
shson <sunghyun7895@naver.com>
2024-05-22 19:54:20 +0900

    리팩토링 v1.5 - 헤더 input박스 내 url 디코딩 개선

--- src/routes/headers/Header.tsx
+++ src/routes/headers/Header.tsx
@@ -149,7 +149,7 @@ function Header(){
                             <option value="bookAuthor">저자</option>
                             <option value="bookName">도서 제목</option>
                         </Select>
-                        <Input type="text" placeholder="도서검색" {...register("inquiryWord")} defaultValue={match ? (word ? word : ""): ""}/>
+                        <Input type="text" placeholder="도서검색" {...register("inquiryWord")} defaultValue={match ? (word ? decodeURI(word) : ""): ""}/>
                         <SearchBtn type="submit">
                             <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></Svg>
                         </SearchBtn>
