4cd06e8b6244b49798cf9243e3b47dd17b826da5
손성현 <sunghyun7895@naver.com>
2024-06-14 22:38:08 +0900

    리팩토링 v3.6 - 도서 리뷰 작성 후 input 빈값 채우기

--- src/routes/body/public/book/detail/info/review/BookRegReview.tsx
+++ src/routes/body/public/book/detail/info/review/BookRegReview.tsx
@@ -1,29 +1,45 @@
 import { useRecoilValue } from "recoil";
 import { IBookReview } from "./BookReview";
-import { useState } from "react";
+import { HTMLInputTypeAttribute, useRef, useState } from "react";
 import { AuthUserInfoAtom, isLoginSelector } from "../../../../../../../atoms/AuthUserInfo";
 import { useRegReview } from "../../../../../../../hooks/hooks";
 
 function BookRegReview({bookNo}:IBookReview){
     const isLogin = useRecoilValue(isLoginSelector);
     const authUserInfo = useRecoilValue(AuthUserInfoAtom);
-    const [reviewContent,setReviewContent] = useState<string>("");
-    
-    const {mutate:regReviewMutate} = useRegReview();
+    // const [reviewContent,setReviewContent] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
 
-    const clickedReviewReg = ()=>{
-        regReviewMutate.mutate({userNo:authUserInfo.userNo,bookNo:bookNo,reviewContent:reviewContent})
+    const onSuccess = (data:any) =>{    
+        if(data.code === "S00"){
+            alert("리뷰 등록하였습니다.");
+            if(inputRef.current){
+                inputRef.current.value="";
+            }
+        }else{
+            alert(data.msg);
+        }
+        
+    }
+    const {mutate:regReviewMutate} = useRegReview(onSuccess);
+
+    const clickedReviewReg = (e:React.MouseEvent<HTMLButtonElement>)=>{
+        console.log("zmffr");
+        if(inputRef.current){
+            if(inputRef.current.value.trim().length==0){
+                alert("리뷰 내용을 입력해주세요.");
+            }else{
+                regReviewMutate.mutate({userNo:authUserInfo.userNo,bookNo:bookNo,reviewContent:inputRef.current.value});
+            }
+        }
     }
 
-    const writeReviewContent=(e:React.ChangeEvent<HTMLInputElement>)=>{
-        setReviewContent(e.currentTarget.value);
-    }
-
+   
     return (
             isLogin 
             ? 
             <div>
-                <input type="text" onBlur={writeReviewContent}/>
+                <input ref={inputRef} type="text" placeholder="리뷰 내용"/>
                 <button onClick={clickedReviewReg}>등록</button>
             </div>
             :
